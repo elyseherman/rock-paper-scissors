2f719f99b98fa0088dcc281723023045a8e7904c
Elyse Herman <elyse_herman@hotmail.com>
2025-08-28 14:58:46 -0400

    Added MVP UI

--- script.js
+++ script.js
@@ -17,41 +17,62 @@ function getHumanChoice() {
 let computerScore = 0;
 let humanScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 'rock') {
+const results = document.getElementById('results');
 
+function playRound(humanChoice, computerChoice) {
+
+    console.log(humanChoice)
+    let outcome = document.createElement('p');
+
+    if (humanChoice == 'rock') {
         if (computerChoice == 'paper') {
             computerScore++;
-            console.log('You lose! Paper covers rock.')
+            outcome.textContent = `You lose! Paper covers rock You: ${humanScore} Computer: ${computerScore}`
         } else if (computerChoice == 'scissors') {
             humanScore++;
-            console.log('You win! Rock crushes scissors.')
+            outcome.textContent = `You win! Rock crushes scissors. You: ${humanScore} Computer: ${computerScore}`
         } else {
-            console.log('Tie! Play again.')
+            outcome.textContent = 'Tie! Play again.'
         }
 
     } else if (humanChoice == 'paper') {
 
         if (computerChoice == 'rock') {
             humanScore++;
-            console.log('You win! Paper covers rock.')
+            outcome.textContent = `You win! Paper covers rock. You: ${humanScore} Computer: ${computerScore}`
         } else if (computerChoice == 'scissors') {
             computerScore++;
-            console.log('You lose! Scissors cut paper.')
+            outcome.textContent = `You lose! Scissors cut paper. You: ${humanScore} Computer: ${computerScore}`
         } else {
-            console.log('Tie! Play again.')
+            outcome.textContent = 'Tie! Play again.'
         }
 
     } else if (humanChoice == 'scissors') {
         if (computerChoice == 'paper') {
             humanScore++;
-            console.log('You win! Scissors cuts paper.')
+            outcome.textContent = `You win! Scissors cuts paper. You: ${humanScore} Computer: ${computerScore}`
         } else if (computerChoice == 'rock') {
             computerScore++;
-            console.log('You lose! Rock crushes scissors.')
+            outcome.textContent = `You lose! Rock crushes scissors. You: ${humanScore} Computer: ${computerScore}`
         } else {
-            console.log('Tie! Play again.')
+            outcome.textContent = 'Tie! Play again.'
         }
+    }
+
+    results.appendChild(outcome)
+
+    if (computerScore == 5) {
+        let winner = document.createElement('p')
+        winner.textContent = 'Computer wins!'
+        results.appendChild(winner)
+        computerScore = 0;
+        humanScore = 0;
+    } else if (humanScore == 5) {
+        let winner = document.createElement('p')
+        winner.textContent = 'You win!'
+        results.appendChild(winner)
+        computerScore = 0;
+        humanScore = 0;
     }
 }
 
@@ -68,4 +89,10 @@ function playGame() {
     }
 }
 
-playGame()
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playRound(e.target.id, getComputerChoice())
+    })
+})
